0b52799b137e75f6ec5e0c81ff5501056ebe94ff
Daniel Levi <daniellevi000@gmail.com>
2026-08-15 10:16:43 -0300

    refactory: move guards para pasta global

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module, type MiddlewareConsumer, type NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule,  ConfigService } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ProxyModule } from './proxy/proxy.module';
 import { MiddlewareModule } from './middleware/middleware.module';
@@ -15,23 +15,27 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ThrottlerModule.forRoot([
-      {
-        name: 'short',
-        ttl: 1000, // 1 second
-        limit: 10, // 10 requests per minute
-      },
-      {
-        name: 'medium',
-        ttl: 60000, // 1 minute
-        limit: 100, // 100 requests per minute
-      },
-      {
-        name: 'long',
-        ttl: 900000, // 15 minute
-        limit: 1000, // 1000 requests per minute
-      },
-    ]),
+     ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => [
+        {
+          name: 'short',
+          ttl: 1000, // 1 second
+          limit: configService.get<number>('RATE_LIMIT_SHORT', 10), // 10 requests per minute
+        },
+        {
+          name: 'medium',
+          ttl: 60000, // 1 minute
+          limit: configService.get<number>('RATE_LIMIT_MEDIUM', 100), // 100 requests per minute
+        },
+        {
+          name: 'long',
+          ttl: 900000, // 15 minute
+          limit: configService.get<number>('RATE_LIMIT_LONG', 1000), // 1000 requests per minute
+        },
+      ],
+      inject: [ConfigService],
+    }),
     ProxyModule,
     MiddlewareModule,
     AuthModule,
