c6fc194d4660abcd7cd004c857a0ffb3837d403a
Daniel Levi <daniellevi000@gmail.com>
2026-08-18 21:35:30 -0300

    feat: :sparkles: adiciona servico de payment

---
+++ payments-service/src/events/events.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PaymentConsumerService } from './payment-consumer/payment-consumer.service';
+import { PaymentQueueService } from './payment-queue/payment-queue.service';
+import { RabbitmqService } from './rabbitmq/rabbitmq.service';
+import { DlqService } from './dlq/dlq.service';
+import { DlqController } from './dlq/dlq.controller';
+import { MetricsController } from './metrics/metrics.controller';
+
+@Module({
+  imports: [ConfigModule],
+  controllers: [DlqController, MetricsController],
+  providers: [
+    RabbitmqService,
+    PaymentQueueService,
+    PaymentConsumerService,
+    DlqService,
+  ],
+})
+export class EventsModule {}
